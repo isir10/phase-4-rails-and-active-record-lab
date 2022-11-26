faa93f1aa972dee84cf058fe95971e4b4e01def1
isir10 <isir.malak@moringaschool.com>
2022-11-26 14:58:21 +0300

    completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Student.create!(first_name:'Isir', last_name: 'Malak', grade:99)
+Student.create!(first_name:'Amina', last_name: 'Raksan', grade:95)
+Student.create!(first_name:'Fardosa', last_name: 'Ali', grade:90)
